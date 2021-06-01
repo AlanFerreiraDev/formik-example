ee15a6ae73b7e6d5ace3243f53fbc8801d119daf
Alan <alan.silva@kaad.com.br>
2021-06-01 11:51:41 -0300

    Validação Pronta no Formik

--- src/App.js
+++ src/App.js
@@ -1,16 +1,53 @@
+import React from "react";
+import { Formik, Field, Form } from "formik";
+
+import "./App.css";
 
 function App() {
+  function onSubmit(values, actions) {
+    console.log("SUBMIT", values);
+  }
+
+  function validate(values) {
+    const errors = {};
+    if (!values.name) {
+      errors.name = "Nome é Obrigatório";
+    }
+
+    if (!values.email) {
+      errors.email = "Email é Obrigatório";
+    }
+
+    return errors;
+  }
+
   return (
     <div className="App">
-      <form>
-        <div>
-          <label htmlFor="">
-            Nome
-          </label>
-          <input type="text" />
-        </div>
-      </form>
-     
+      <Formik
+        validate={validate}
+        onSubmit={onSubmit}
+        initialValues={{
+          name: "",
+          email: "",
+        }}
+        render={({ values, errors }) => (
+          // Assim não precisomais passar o handleSubmit no Form
+          <Form>
+            <div>
+              <label>Nome: </label>
+              {/* Com o Field eu não preciso mais passar value, e nem o onChange */}
+              <Field name="name" type="text" />
+              {errors.name && <span>{errors.name}</span>}
+            </div>
+            <div>
+              <label>Email: </label>
+              <Field name="email" type="email" />
+              {errors.email && <span>{errors.email}</span>}
+            </div>
+            <button type="submit">Enviar</button>
+          </Form>
+        )}
+      />
     </div>
   );
 }
